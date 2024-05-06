8238a8377ff583d373e6f28d03666b4e28d88589
Teolin Codreanu <t.codreanu@rankingcoach.com>
2024-05-06 22:15:16 +0300

    feat: stable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TaskModule } from './modules/task/task.module';
-import { ListModule } from './modules/list/list.module';
-import { UserModule } from './modules/user/user.module';
-import { DatabaseService } from './modules/database/database.service';
 
 @Module({
-  imports: [TaskModule, ListModule, UserModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, DatabaseService],
+  providers: [AppService],
 })
 export class AppModule {}
