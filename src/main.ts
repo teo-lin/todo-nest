83fa6e143dc5b9b0337db2b3b472cd15aa01d93d
Teolin Codreanu <t.codreanu@rankingcoach.com>
2024-05-11 22:41:20 +0300

    feat: 404

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // MIDDLEWARE
+  app.useGlobalFilters({
+    catch: (exception, host) =>
+      host.switchToHttp().getResponse().status(404).json({ message: 'Route not found' }),
+  });
+
+  // SERVER
   await app.listen(3333);
 }
 bootstrap();
