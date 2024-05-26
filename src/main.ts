c750e71b80c53ceea47f9f71fc8841b10a0d485b
Teolin <teolin@gmail.com>
2024-05-26 14:17:04 +0300

    chore: /api

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // MIDDLEWARE
+  app.setGlobalPrefix('api');
   app.useGlobalFilters({
     catch: (exception, host) =>
       host.switchToHttp().getResponse().status(404).json({ message: 'Route not found' }),
