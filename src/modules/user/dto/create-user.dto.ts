52aae4984a8e325aeb7b3a0211d0a1aeeddbc82d
teolin <teolin@gmail.com>
2026-01-11 18:15:45 +0200

    chore: bump node

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsString, IsNotEmpty, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
   fullname: string;
 }
